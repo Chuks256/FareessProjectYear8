a29533124745d7ea5ba7ee199818412ec1e7a03d
chuks <alexanderchuks310@gmail.com>
2023-06-24 13:58:24 +0100

    finally

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 
 const express=require("express")
 const app=express();
-const bodyParser=require("bodyParser");
 const port=4444;
 const path=require("path")
 
 app.disable("x-powered-by");
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
-app.use("public",path.join(__dirname,'public'));
+app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"/public/Index.html"))
